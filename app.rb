2fa78e10144703c66d7d21a51c33e94fcafb704a
Alex Jegtnes <saccosekk@gmail.com>
2013-04-21 21:20:04 +0100

    very, very basic Sinatra stuff up

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
 get '/' do
-  'Hello, space. SPACECATS GO HERE.'
+  '<h1>Hello, space. SPACECATS GO HERE. And shit.</h1>'
 end 
+
+
+# dimensions
+get '/*/*' do
+  "<h1>You want a placeholder image. Woo!</h1><pre>#{params[:splat].inspect}</pre>"
+end
