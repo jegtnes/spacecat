9d72e16225c776a041f0087461135d03f26378ee
Alex Jegtnes <saccosekk@gmail.com>
2013-05-05 11:43:08 +0100

    Acquiring a random image now works

--- app.rb
+++ app.rb
@@ -2,10 +2,26 @@ require 'sinatra'
 
 get '/' do
   '<h1>Hello, space. SPACECATS GO HERE. And shit.</h1>'
-end 
-
-
-# dimensions
-get '/*/*' do
-  "<h1>You want a placeholder image. Woo!</h1><pre>#{params[:splat].inspect}</pre>"
 end
+
+get '/image' do
+  # sample picks a random array item
+  file = Dir.glob("public/img/*.jpg").sample.to_s
+
+  "<img src='#{file.sub! 'public/', ''}' alt='SPACECAT'>"
+end
+
+# # Square image
+# get '/:length' do
+#   "You want a #{params[:length]}x#{params[:length]} placeholder image. Woo!"
+# end
+
+# # Image, no filters
+# get '/:width/:height' do
+#   "You want a #{params[:width]}x#{params[:height]} placeholder image. Woo!"
+# end
+
+# # Image, filters
+# get '/:width/:height/:filter' do
+#   "You want a #{params[:width]}x#{params[:height]} placeholder image, with a <strong>#{params[:filter]} filter. Woo!"
+# end
